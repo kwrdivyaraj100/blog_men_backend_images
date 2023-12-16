794581d46b7ed95345d542a5e673d101fcd408a7
Divya R <kwrdivyaraj100@gmail.com>
2023-12-16 15:15:55 +0530

    Initializing Express server with user and blog routes, and MongoDB connection

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const userRouter = require('./routes/user-routes.js');
+const blogRouter = require('./routes/blog-routes.js');
+const cors = require('cors');
 
 
 
 const app = express();
+app.use(cors());
 app.use(express.json());
+app.use("/api/user", userRouter);
+app.use("/api/blog", blogRouter);
 mongoose
-  .connect(
-    "mongodb+srv://admin:divya@cluster0.utimwbh.mongodb.net/?retryWrites=true&w=majority"
-  )
-  .then(() => app.listen(5000))
-  .then(() =>
-    console.log("Connected TO Database and Listening TO Localhost 5000")
-  )
-  .catch((err) => console.log(err));
+    .connect(
+        "mongodb+srv://admin:divya@cluster0.utimwbh.mongodb.net/?retryWrites=true&w=majority"
+    )
+    .then(() => app.listen(5000))
+    .then(() =>
+        console.log("Connected TO Database and Listening TO Localhost 5000")
+    )
+    .catch((err) => console.log(err));

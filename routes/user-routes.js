9d2821fb499f725c6422cbd136ece45c160f80f9
Divya R <kwrdivyaraj100@gmail.com>
2023-12-16 16:00:19 +0530

    SignUp completed

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,8 +1,9 @@
-const express = require('express') ;
-const getAllUser  = require("../controllers/user-controller.js");
-
+const express = require('express');
+const getAllUser = require("../controllers/user-controller.js");
+const signup = require("../controllers/user-controller.js");
 const router = express.Router();
 
 router.get("/", getAllUser);
+router.post("/signup", signup);
 
-module.exports =router;
+module.exports = router;
